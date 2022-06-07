2837b7d7dab85b3438d032efc3da0347d077bad1
Anushaphullathi <96220359+Anushaphullathi@users.noreply.github.com>
2022-06-07 16:52:44 +0000

    done!

--- ActivitySet#3/problem#1.py
+++ ActivitySet#3/problem#1.py
@@ -1,9 +1,7 @@
-import math
 a = int(input())
 for i in range(a):
-  l = list(map(float, input().split()))
-  t = math.sqrt((l[0][0] - l[0][1])**2+(l[1][0] - l[1][1]**2))
-  h = math.sqrt((l[2][0] - l[3][0])**2 + (l[2][1] - l[3][1])**2)
-  area = t*h
-  print("Area of rectangle with vertices",l[0],l[1],l[2],l[3],"is",area)
+  x1,y1,x2,y2,x3,y3= map(float, input().split())
+  area=abs(x1*(y2-y3)+x2*(y3-y1)+x3*(y2-y1))
+  print("Area of rectangle with vertices",(x1,y1),(x2,y2),(x3,y3),"is",area)
 #Area of rectangle with vertices (0.0,0.0),(0.0,1.0),(1.0,0.0) is 1.0
+#ActivitySet#3/problem#1.py
