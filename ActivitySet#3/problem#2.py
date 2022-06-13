d0eabc81a3a0f85cd25a72f3896848f4fc7e3232
Anushaphullathi <96220359+Anushaphullathi@users.noreply.github.com>
2022-06-13 04:55:13 +0000

    done!!!!

--- ActivitySet#3/problem#2.py
+++ ActivitySet#3/problem#2.py
@@ -1,5 +1,5 @@
 #python ActivitySet#3/problem#2.py
-a = int(input())
+'''''a = int(input())
 for i in range(a):
     b=int(input())
     x=list(map(int,input().split()))
@@ -7,6 +7,30 @@ for i in range(a):
     for k in range(b-1):
       sum=sum+(1/x[0+k])
       #for l in range(b):
-    print(f"{x} = {sum}")
-  #two for loops are used
-  c
+    print(f"{x} = {sum}")'''''
+import math
+def get_fraction(l):
+    num=0
+    den=1
+    for i in l:
+      den*=i
+    for i in l:
+      num+=den/i
+    gcd=math.gcd(int(den),int(num))
+    return([int(num/gcd),int(den/gcd)])
+
+def get_num():
+    a=int(input())
+    for i in range(a):
+      b=int(input())
+      x=list(map(int,input().split()))
+      k=get_fraction(x)
+      for i in x[:-1]:
+        print(f"1/{i} ",end="+ ")
+      print(f"1/{x[b-1]} = {k[0]}/{k[1]}")
+get_num()
+
+
+    
+
+  
