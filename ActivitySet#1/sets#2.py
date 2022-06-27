0b3e5d401359a4e4868500343e23c068071ab5d7
Anushaphullathi <96220359+Anushaphullathi@users.noreply.github.com>
2022-06-27 05:13:07 +0000

    set2 yet

---
+++ ActivitySet#1/sets#2.py
@@ -0,0 +1,45 @@
+# make this code work
+'''def main()
+    setu = input_set()
+    set1 = input_set()
+    set2 = input_set()
+    set3 = input_set3()
+    operation = get_operation()
+    operands = get_operands()
+    result = perform_operation(operation,*operands)
+    print(result)
+    print_report(setu,set1,set2,set3) # members in only s1 and s2, members in only s2,s3, members in only s3,s1, members not in s1,s2,s3 and members in all s1,s2,s3 '''
+def input_set():
+  return((input("Enter set elements ")).split(" "))
+
+def get_operation(s1,s2,op):
+  op = input("u or i")
+  if op in ['u','i']:
+    return op
+  else:
+    return none
+    
+def get_operands():
+  set = ['setu','set1','set2','set3']
+  oper1 = input('which set?')
+  if oper1 in set:
+    
+  oper2 = input('which set?')
+  if oper1 in get_operation():
+    
+     
+def main()
+    setu = input_set()
+    set1 = input_set()
+    set2 = input_set()
+    set3 = input_set()
+    operation = get_operation()
+    operands = get_operands()
+    result = perform_operation(operation,*operands)
+    print(result)
+    print_report(setu,set1,set2,set3)
+
+
+
+  
+  
