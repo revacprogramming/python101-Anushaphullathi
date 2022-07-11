6496d92ec8f584765e03c7a7ec1179ca3498ede7
Anushaphullathi <96220359+Anushaphullathi@users.noreply.github.com>
2022-07-11 17:46:39 +0000

    yet to !

--- ActivitySet#1/problem#14.py
+++ ActivitySet#1/problem#14.py
@@ -49,3 +49,16 @@ while True:
     sum = tree.findall('contents')
     sum+=sum
 print(sum)
+
+#Json
+import json
+import urllib.request, urllib.parse, urllib.error
+sum = 0
+fhand = urllib.request.uropen('http://py4e-data.dr-chuck.net/comments_42.json')
+info = json.loads(fhand)
+print('User count:', len(info))
+for item in info:
+    sum = info.
+    sum+=sum
+print(sum)
+    
