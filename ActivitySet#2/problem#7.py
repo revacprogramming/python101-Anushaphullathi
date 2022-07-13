e1a9e5a966110ec72fa1f1f85a4c61d171393dd0
Anushaphullathi <96220359+Anushaphullathi@users.noreply.github.com>
2022-07-13 09:51:32 +0000

    __str__

--- ActivitySet#2/problem#7.py
+++ ActivitySet#2/problem#7.py
@@ -1,10 +1,28 @@
 
-
 class Menu:
     """fill in class definition here"""
+    def __init__(self):
+        self.list = dict()
+
+
+    def add(self,x,n):
+        if(x in self.list):
+            self.list[x]+=n
+        else:
+            self.list[x]=n
+    def show(self):
+         for i in self.list:
+             print(i,self.list)
+
+    def __str__(self):
+         s = ""
+         for i in self.list:
+             s+=i+" "+str(self.list[i])+"\n"
+         return(s[:-1])
 
 
 m = Menu()
-m = m + ("idly", 10) + ("vada", 20)  # Hint: operator overloading special methods (__add__, __sub__, etc.)
+m.add('idly',10)
+m.add('vada',20)
 
-print(m)  # should print the menu properly
+print(m) # should print the menu properly
