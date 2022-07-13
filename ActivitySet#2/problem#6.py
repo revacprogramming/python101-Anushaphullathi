2c9d3c4906009a8f6b07d2153a5b3f56c90ac814
Anushaphullathi <96220359+Anushaphullathi@users.noreply.github.com>
2022-07-13 09:35:13 +0000

    done!!

--- ActivitySet#2/problem#6.py
+++ ActivitySet#2/problem#6.py
@@ -1,11 +1,18 @@
-
-
 class Menu:
     """fill in class definition here"""
-  def __init__(self,idly,vada):
-    self.idly = idly
-    self.vada = vada
-    
+  def __init__(self):
+   self.list = dict()
+
+  def add(self,x,n):
+    if(x in self.list()):
+      self.list[x]+=n
+    else:
+      self.list():
+      self.list[x]=n
+  def show(self):
+    for i in self.list():
+      print(i,self.list())
+      
 m = Menu()  # Menu is a class#m is object
 m.add("idly", 10)
 m.add("vada", 20)
