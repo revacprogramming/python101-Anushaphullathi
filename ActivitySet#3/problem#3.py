0b8584aeb99293317e6ea453b96e9b6d5ba01160
Anushaphullathi <96220359+Anushaphullathi@users.noreply.github.com>
2022-07-18 04:57:40 +0000

    still have to

--- ActivitySet#3/problem#3.py
+++ ActivitySet#3/problem#3.py
@@ -1,7 +1,37 @@
-a=int(input())
+a = int(input())
 for i in range(a):
-  x=input()
-  for letter in x:
-  #loop it
+  b =int(input())
+  s = input()
+  for j in s:
     
+  
+  
+  
     
+    Sample Input
+------------
+3
+7
+baabbab
+5
+bbacb
+10
+aabababaab
+Resultant Output
+----------------
+baabbab
+1 baab
+3 abba 5 bab
+bbacb
+aabababaab
+1 aabababaa
+2 aba
+2 ababa
+2 abababa
+3 bab
+3 babab
+4 aba
+4 ababa
+5 bab
+6 aba
+7 baab
